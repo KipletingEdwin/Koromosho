ee4f9f20e11fd3572244153e1b06806b9cfc361d
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-02-14 09:07:24 +0300

    remove background colors

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,50 +1,52 @@
-import React, {useState} from 'react'
-import styles from './Navbar.module.css'
-import closeIcon from '../../assets/nav/closeIcon.png';
-import menuIcon from '../../assets/nav/menuIcon.png';
-
+import React, { useState } from "react";
+import styles from "./Navbar.module.css";
+import closeIcon from "../../assets/nav/closeIcon.png";
+import menuIcon from "../../assets/nav/menuIcon.png";
 
 function Navbar() {
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
-
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   return (
     <div className={styles.container}>
-        <nav className={styles.navbar}>
-            <a href='/' className={styles.title}>ACK ST. JAMES KOROMOSHO</a>
-            <div className={styles.menu}>
-            <img src={isMenuOpen ? menuIcon : closeIcon} alt='menu' onClick={() => setIsMenuOpen(!isMenuOpen)}/>
-                <ul className={styles.menuList}>
-                    <li>
-                        <a href='/'>Home</a>
-                    </li>
+      <nav className={styles.navbar}>
+        <a href="/" className={styles.title}>
+          ACK ST. JAMES KOROMOSHO
+        </a>
+        <div className={styles.menu}>
+          <img
+            src={isMenuOpen ? menuIcon : closeIcon}
+            alt="menu"
+            onClick={() => setIsMenuOpen(!isMenuOpen)}
+          />
+          <ul className={`${styles.menuList} ${isMenuOpen && styles.menuOpen}`} onClick={() => setIsMenuOpen(false)}>
+            <li>
+              <a href="/">Home</a>
+            </li>
 
-                    <li>
-                        <a href='/about'>About</a>
-                    </li>
+            <li>
+              <a href="/about">About</a>
+            </li>
 
-                    <li>
-                        <a href='/events'>Events</a>
-                    </li>
+            <li>
+              <a href="/events">Events</a>
+            </li>
 
-                    <li>
-                        <a href='/sermons'>Sermons</a>
-                    </li>
+            <li>
+              <a href="/sermons">Sermons</a>
+            </li>
 
-                    <li>
-                        <a href='/news'>News</a>
-                    </li>
+            <li>
+              <a href="/news">News</a>
+            </li>
 
-                    <li>
-                        <a href='/contact'>Contact</a>
-                    </li>
-                </ul>
-
-            </div>
-
-        </nav>
+            <li>
+              <a href="/contact">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
