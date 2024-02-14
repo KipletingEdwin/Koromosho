a16173af85733b056f680220dcfd6f9355dedb6f
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-02-14 16:55:52 +0300

    add background colors

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -6,9 +6,12 @@ function Home() {
   return (
     <section className={styles.container}>
       <img className={styles.backImage} src={koroProfile} alt="koro" />
-     <div className={styles.content}>
-     <h1 className={styles.title}> ACK ST. JAMES KOROMOSHO</h1>
-     </div>
+      <div className={styles.content}>
+        <div className={styles.contentTitle}>
+          <h1 className={styles.title}> ACK ST. JAMES KOROMOSHO</h1>
+        </div>
+        <h4>-UPCOMING SERVICE-</h4>
+      </div>
     </section>
   );
 }
