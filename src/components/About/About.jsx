241ea6ccfd8d8003ba9cab3848822441b64a132b
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-02-15 12:38:32 +0300

    add About folder

---
+++ src/components/About/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './About.module.css';
+
+function About() {
+  return (
+    <section className={styles.container}>
+        <h1> About</h1>
+    </section>
+  )
+}
+
+export default About
