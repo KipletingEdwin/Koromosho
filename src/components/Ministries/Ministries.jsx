3e3b23750f10f374ab67009b0b3accec0070d51a
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-02-16 18:22:40 +0300

    add different ministries and their images

--- src/components/Ministries/Ministries.jsx
+++ src/components/Ministries/Ministries.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 import styles from "./Ministries.module.css";
+import kama from '../../assets/ministry/KAMA_LOGO.png';
+import mu from '../../assets/ministry/mothersunionlogo.jpg';
+import kayo from '../../assets/ministry/kayo.png';
+import choir from '../../assets/ministry/choir.png';
 
 function Ministries() {
   return (
@@ -8,6 +12,65 @@ function Ministries() {
         <hr className={styles.line} />
         <p>Ministries</p>
         <hr className={styles.line} />
+      </div>
+      <div className={styles.departments}>
+        <div className={styles.departmentsContainer}>
+            <div className={styles.kama}>
+                <img src={kama} alt="kama"/>
+                <div className={styles.description}>
+                    <h3> Kenya Anglican Men Association(KAMA)</h3>
+                    <hr className={styles.kamaLine} />
+                    <p>Learn More</p>
+                </div>
+            </div>
+
+            <div className={styles.kama}>
+                <img src={mu} alt="kama"/>
+                <div className={styles.description}>
+                    <h3> Mothers' Union</h3>
+                    <hr className={styles.kamaLine} />
+                    <p>Learn More</p>
+                </div>
+            </div>
+
+            <div className={styles.kama}>
+                <img src={kayo} alt="kama"/>
+                <div className={styles.description}>
+                    <h3> Kenya Anglican Youth Organization(KAYO)</h3>
+                    <hr className={styles.kamaLine} />
+                    <p>Learn More</p>
+                </div>
+            </div>
+
+            <div className={styles.kama}>
+                <img src={choir} alt="kama"/>
+                <div className={styles.description}>
+                    <h3> Children's and Teen's Ministry</h3>
+                    <hr className={styles.kamaLine} />
+                    <p>Learn More</p>
+                </div>
+            </div>
+
+            <div className={styles.kama}>
+                <img src={choir} alt="kama"/>
+                <div className={styles.description}>
+                    <h3> Choir</h3>
+                    <hr className={styles.kamaLine} />
+                    <p>Learn More</p>
+                </div>
+            </div>
+
+            <div className={styles.kama}>
+                <img src={kama} alt="kama"/>
+                <div className={styles.description}>
+                    <h3> Cell Groups</h3>
+                    <hr className={styles.kamaLine} />
+                    <p>Learn More</p>
+                </div>
+            </div>
+
+        </div>
+
       </div>
     </section>
   );
