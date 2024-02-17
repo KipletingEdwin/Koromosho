5475d8a686e37653a1548d6e93c921e53fadba65
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-02-17 16:29:09 +0300

    add connect form inputs

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,13 +1,46 @@
-import React from 'react'
-import styles from './Contact.module.css';
+import React from "react";
+import styles from "./Contact.module.css";
 
 function Contact() {
   return (
     <section className={styles.container}>
-        <h1> Connect</h1>
+      <div className={styles.containerTop}>
+        <hr className={styles.line} />
+        <p>Contact Us</p>
+        <hr className={styles.line} />
+      </div>
+      <div className={styles.contactContainer}>
+        <div className={styles.contactForm}>
+            <p>Drop us a message</p>
+            <form>
+                <ul>
+                    <li> 
+                    <input type="text" placeholder="Name" name="name" required/>
+                    </li>
 
+                    <li> 
+                    <input type="email" placeholder="Email" name="email" required/>
+                    </li>
+
+                    <li> 
+                    <input type="tel" placeholder="Phone Number" name="phone" required/>
+                    </li>
+
+                    <li> 
+                    <textarea placeholder="Message" name="message" required>
+                        
+                    </textarea>
+                    </li>
+                </ul>
+
+            </form>
+          
+
+        </div>
+
+      </div>
     </section>
-  )
+  );
 }
 
-export default Contact
+export default Contact;
