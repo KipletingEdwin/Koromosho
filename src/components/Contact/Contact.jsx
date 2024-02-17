506815936a6957694407f443dacca9a8da5efb6b
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-02-17 17:06:45 +0300

    add fontawesome icons

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import styles from "./Contact.module.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook, faXTwitter, faInstagram } from '@fortawesome/free-solid-svg-icons';
 
 function Contact() {
   return (
@@ -11,30 +13,56 @@ function Contact() {
       </div>
       <div className={styles.contactContainer}>
         <div className={styles.contactForm}>
-            <p>Drop us a message</p>
-            <form>
-                <ul>
-                    <li> 
-                    <input type="text" placeholder="Name" name="name" required/>
-                    </li>
+          <p>Drop us a message</p>
+          <form>
+            <ul>
+              <li>
+                <input type="text" placeholder="Name" name="name" required />
+              </li>
 
-                    <li> 
-                    <input type="email" placeholder="Email" name="email" required/>
-                    </li>
+              <li>
+                <input type="email" placeholder="Email" name="email" required />
+              </li>
 
-                    <li> 
-                    <input type="tel" placeholder="Phone Number" name="phone" required/>
-                    </li>
+              <li>
+                <input
+                  type="tel"
+                  placeholder="Phone Number"
+                  name="phone"
+                  required
+                />
+              </li>
 
-                    <li> 
-                    <textarea placeholder="Message" name="message" required>
-                        
-                    </textarea>
-                    </li>
-                </ul>
+              <li>
+                <textarea
+                  placeholder="Message"
+                  name="message"
+                  required
+                ></textarea>
+              </li>
+            </ul>
+          </form>
+        </div>
+      </div>
+      <div className={styles.contact}>
+        <div className={styles.contactInfo}>
+            <div className={styles.address}>
+                <p> Call us: +254712345678 </p>
+                <p> Email: ackstjameskoromosho@gmail.com</p>
+            </div>
+            <div className={styles.socials}>
+                <h3> Follow Us:</h3>
+                <div className={styles.icons}>
+                    
+                    <a href="/facebook.com">  <FontAwesomeIcon icon={faXTwitter} /> </a>
+                    <a href="/facebook.com">  <FontAwesomeIcon icon={faFacebook} /> </a>
+                    <a href="/facebook.com">  <FontAwesomeIcon icon={faInstagram} /> </a>
+               
+               
 
-            </form>
-          
+                </div>
+
+            </div>
 
         </div>
 
