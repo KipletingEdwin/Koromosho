f8dd1a2679af7e2e11711119821c16a9459a1f44
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-02-18 19:21:45 +0300

    style input form

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from "./Contact.module.css";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFacebook, faXTwitter, faInstagram } from '@fortawesome/free-solid-svg-icons';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faFacebook, faXTwitter, faInstagram } from '@fortawesome/free-solid-svg-icons';
 
 function Contact() {
   return (
@@ -35,15 +35,16 @@ function Contact() {
 
               <li>
                 <textarea
-                  placeholder="Message"
+                  placeholder="Your Message"
                   name="message"
                   required
                 ></textarea>
               </li>
+              <button className={styles.contactBtn}>SEND</button>
             </ul>
           </form>
         </div>
-      </div>
+     
       <div className={styles.contact}>
         <div className={styles.contactInfo}>
             <div className={styles.address}>
@@ -53,19 +54,16 @@ function Contact() {
             <div className={styles.socials}>
                 <h3> Follow Us:</h3>
                 <div className={styles.icons}>
-                    
-                    <a href="/facebook.com">  <FontAwesomeIcon icon={faXTwitter} /> </a>
+                    {/* <a href="/facebook.com">  <FontAwesomeIcon icon={faXTwitter} /> </a>
                     <a href="/facebook.com">  <FontAwesomeIcon icon={faFacebook} /> </a>
-                    <a href="/facebook.com">  <FontAwesomeIcon icon={faInstagram} /> </a>
-               
-               
-
+                    <a href="/facebook.com">  <FontAwesomeIcon icon={faInstagram} /> </a> */}
                 </div>
 
             </div>
 
         </div>
 
+      </div>
       </div>
     </section>
   );
