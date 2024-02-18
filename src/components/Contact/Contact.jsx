4a5dc4f736b85578d11b81fbd8404b159465f472
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-02-18 20:48:55 +0300

    add bullseye icon

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from "./Contact.module.css";
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import { faFacebook, faXTwitter, faInstagram } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBullseye } from '@fortawesome/free-solid-svg-icons';
 
 function Contact() {
   return (
@@ -44,26 +44,28 @@ function Contact() {
             </ul>
           </form>
         </div>
-     
-      <div className={styles.contact}>
-        <div className={styles.contactInfo}>
+
+        <div className={styles.contact}>
+          <div className={styles.contactInfo}>
             <div className={styles.address}>
-                <p> Call us: +254712345678 </p>
-                <p> Email: ackstjameskoromosho@gmail.com</p>
+                <div className={styles.eyeContact}>
+                <FontAwesomeIcon icon={faBullseye} className={styles.icons} />
+                <h3>Contact Information</h3>
+                </div>
+             <h3>Contact Information</h3>
+              <p> Call us: +254712345678 </p>
+              <p> Email: ackstjameskoromosho@gmail.com</p>
             </div>
             <div className={styles.socials}>
-                <h3> Follow Us:</h3>
-                <div className={styles.icons}>
-                    {/* <a href="/facebook.com">  <FontAwesomeIcon icon={faXTwitter} /> </a>
+              <h3> Follow Us:</h3>
+              <div className={styles.icons}>
+                {/* <a href="/facebook.com">  <FontAwesomeIcon icon={faXTwitter} /> </a>
                     <a href="/facebook.com">  <FontAwesomeIcon icon={faFacebook} /> </a>
                     <a href="/facebook.com">  <FontAwesomeIcon icon={faInstagram} /> </a> */}
-                </div>
-
+              </div>
             </div>
-
+          </div>
         </div>
-
-      </div>
       </div>
     </section>
   );
