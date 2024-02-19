fa82c6090da4a8b28bcd05a92729e2bc9e134aa2
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-02-19 10:07:39 +0300

    style the a tags in copyright section

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,21 +4,20 @@ import styles from "./Footer.module.css";
 function Footer() {
   return (
     <section className={styles.container}>
-        <h2>Footer</h2>
       <div className={styles.footerLine}>
         <hr className={styles.line} />
       </div>
 
       <div className={styles.bottomFooter}>
         <div className={styles.rights}>
-        <p>&copy; Copyright 2019. All rights reserved.</p>
+          <p>&copy; Copyright <span className={styles.copyright}>  <a href="https://www.ackenya.org/">Anglican Church of Kenya</a> </span></p>
+          <p> All rights reserved.</p>
         </div>
 
         <div className={styles.terms}>
-            <a href="">Terms & Conditions</a>
-            <a href="">Private Policy</a>
+          <a href="">Terms & Conditions</a>
+          <a href="">Private Policy</a>
         </div>
-
       </div>
     </section>
   );
