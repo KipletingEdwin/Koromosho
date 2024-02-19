c9a6ae9da7e398734d947cf0cc5d4ff07dcfaefa
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-02-19 09:43:56 +0300

    add terms and conditions

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,13 +1,27 @@
-import React from 'react';
-import styles from './Footer.module.css';
+import React from "react";
+import styles from "./Footer.module.css";
 
 function Footer() {
   return (
     <section className={styles.container}>
-        <h2> Footer</h2>
+        <h2>Footer</h2>
+      <div className={styles.footerLine}>
+        <hr className={styles.line} />
+      </div>
 
+      <div className={styles.bottomFooter}>
+        <div className={styles.rights}>
+        <p>&copy; Copyright 2019. All rights reserved.</p>
+        </div>
+
+        <div className={styles.terms}>
+            <a href="">Terms & Conditions</a>
+            <a href="">Private Policy</a>
+        </div>
+
+      </div>
     </section>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
