ca141d2cb57d6e1b64e21159b7a8f1fb38bd78ee
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-02-26 09:34:15 +0300

    regroup core values to json file

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import styles from "./About.module.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEye, faBullseye } from '@fortawesome/free-solid-svg-icons';
+import projects from '../../Data/Data.json'
 
 
 function About() {
@@ -50,31 +51,18 @@ function About() {
       <div className={styles.faith}>
         <h3 className={styles.coreValuesTitle}>Core Values</h3>
         <div className={styles.coreValues}>
-          <div className={styles.value}>
-            <h3>Faith in Christ</h3>
-            <p> Demonstrating a deep and committed acknowledgement of Christ and his teachings in all our work and engagement. </p>
-          </div>
 
-          <div className={styles.value}>
-            <h3>Love</h3>
-            <p> Sharing the Love of Christ with the world. </p>
-          </div>
+          {
+            projects.map((project, index)=>{
+              return (
+              <div key={index} className={styles.value}>
+                <h3> {project.title} </h3>
+                <p> {project.Description} </p>
 
-          <div className={styles.value}>
-            <h3>Integrity</h3>
-            <p> Being good and responsible stewards of Church resources and ensuring prudent management of the same, through 
-              establishment of well-defined structures and policies. </p>
-          </div>
-
-          <div className={styles.value}> 
-            <h3>Honor</h3>
-            <p>Honoring God in our Work and service and engagement with the World. </p>
-          </div>
-
-          <div  className={styles.value}>
-            <h3>Professionalism</h3>
-            <p> Being professional in all our service and maintaining good relations at all levels. </p>
-          </div>
+              </div>
+              )
+            })
+          }
 
         </div>
 
