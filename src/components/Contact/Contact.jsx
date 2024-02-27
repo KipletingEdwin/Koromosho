c891989163d2115a3066f3c4c7a82907889bf8be
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-02-27 08:27:03 +0300

    display felx for footer items

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import styles from "./Contact.module.css";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBullseye, faPhone, faEnvelope, faLocationDot } from '@fortawesome/free-solid-svg-icons';
-import facebook from '../../assets/socials/facebook.png';
-import X from '../../assets/socials/X.png';
-import instagram from '../../assets/socials/Instagram.png';
-
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faBullseye,
+  faPhone,
+  faEnvelope,
+  faLocationDot,
+} from "@fortawesome/free-solid-svg-icons";
+import facebook from "../../assets/socials/facebook.png";
+import X from "../../assets/socials/X.png";
+import instagram from "../../assets/socials/Instagram.png";
 
 function Contact() {
   return (
@@ -53,30 +56,48 @@ function Contact() {
         <div className={styles.contact}>
           <div className={styles.contactInfo}>
             <div className={styles.address}>
-                <div className={styles.eyeContact}>
+              <div className={styles.eyeContact}>
                 <FontAwesomeIcon icon={faBullseye} className={styles.icon} />
                 <h3>Contact Information</h3>
-                </div>
-              <p><FontAwesomeIcon icon={faPhone} /> : +254712345678 </p>
-              <p><FontAwesomeIcon icon={faEnvelope} />:  ackstjameskoromosho@gmail.com</p>
-              <p><FontAwesomeIcon icon={faLocationDot} />: Kaptinga,Simat Location,Kapseret Constituency,Uasin Gishu County</p>
+              </div>
+              <p>
+                <FontAwesomeIcon icon={faPhone} /> : +254712345678{" "}
+              </p>
+              <p>
+                <FontAwesomeIcon icon={faEnvelope} />:
+                ackstjameskoromosho@gmail.com
+              </p>
+              <p>
+                <FontAwesomeIcon icon={faLocationDot} />: Kaptinga,Simat
+                Location,Kapseret Constituency,Uasin Gishu County
+              </p>
             </div>
             <div className={styles.socials}>
               <h3> Follow Us:</h3>
               <div className={styles.socialIcons}>
-              <a href="https://www.facebook.com/groups/282547165136513" target="_blank" rel="noopener noreferrer">
-             <img src={facebook} alt="Facebook" />
-            </a>
+                <a
+                  href="https://www.facebook.com/groups/282547165136513"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img src={facebook} alt="Facebook" />
+                </a>
 
-            <a href="https://www.facebook.com/groups/282547165136513" target="_blank" rel="noopener noreferrer">
-             <img src={X} alt="Twitter" />
-            </a>
+                <a
+                  href="https://www.facebook.com/groups/282547165136513"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img src={X} alt="Twitter" />
+                </a>
 
-
-            <a href="https://www.facebook.com/groups/282547165136513" target="_blank" rel="noopener noreferrer">
-             <img src={instagram} alt="instagram" />
-            </a>
-
+                <a
+                  href="https://www.facebook.com/groups/282547165136513"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img src={instagram} alt="instagram" />
+                </a>
               </div>
             </div>
           </div>
