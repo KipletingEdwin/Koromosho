80c22164f4c525a0f05aaa79765dcedf49a85e71
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-03-11 15:47:29 +0300

    redesign the footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,69 +1,20 @@
 import React from "react";
 import styles from "./Footer.module.css";
-import logo from "../../assets/nav/koro.png";
-import { Link } from "react-router-dom";
 
 function Footer() {
   return (
     <section className={styles.container}>
-       <hr className={styles.line} />
+      <hr className={styles.line} />
       <div className={styles.aboutChurch}>
-        <div className={styles.ack}>
-          <h4>ACK ST JAMES KOROMOSHO</h4>
-          <img src={logo} alt="koromosho" />
-          <p>Welcome to ACK St James Koromosho,feel free to worship with us</p>
+        <p>A.C.K St James Koromosho,Kaptinga,Simat Location</p>
+        <div className={styles.contactFooter}>
+          <p> Tel:0712345678</p>
+          <p> Email:ackstjameskoromosho@gmail.com</p>
+          <p> Web:www.stjameskoromosho.or.ke</p>
         </div>
-        <div className={styles.aboutSection}>
-          <h4>About Church</h4>
-          <ul>
-            <li>
-              <Link to="/home">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/ministries">Ministries</Link>
-            </li>
-            <li>
-              <Link to="/contact">Contact</Link>
-            </li>
-          </ul>
-        </div>
-        <div className={styles.shop}>
-          <h4>Join our email list</h4>
-          <form className={styles.inputContainer}>
-            <input name="email" type="email" placeholder="Email" required />
-          </form>
-          <button>Subscribe</button>
-          <p>
-            By completing this form you are signing up to receive our emails and
-            can unsubscribe at any time
-          </p>
-        </div>
-      </div>
-      <div className={styles.footerLine}>
-        <hr className={styles.line} />
-      </div>
-
-      <div className={styles.bottomFooter}>
-        <div className={styles.rights}>
-          <p>
-            &copy; Copyright{" "}
-            <span className={styles.copyright}>
-              {" "}
-              <a href="https://www.ackenya.org/">
-                Anglican Church of Kenya
-              </a>{" "}
-            </span>
-          </p>
-          <p> All rights reserved.</p>
-        </div>
-
-        <div className={styles.terms}>
-          <a href="">Terms & Conditions</a>
-          <a href="">Private Policy</a>
-        </div>
+        <p>
+          <p>Copyright &copy; 2016- St James Koromosho</p>
+        </p>
       </div>
     </section>
   );
